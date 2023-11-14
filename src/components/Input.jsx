007443af5bfaa25f2466c7ce61a3d931f3a16c67
gogi999 <g.petrovic9@gmail.com>
2023-11-14 16:34:57 +0100

    Added tailwindcss in project

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,4 +1,6 @@
-import { styled } from 'styled-components';
+import React from 'react';
+
+/*import { styled } from 'styled-components';
 
 const Label = styled.label`
   display: block;
@@ -29,3 +31,25 @@ export default function CustomInput({ label, invalid, ...props }) {
     </p>
   );
 }
+*/
+
+export default function Input({ label, invalid, ...props }) {
+    let labelClasses = 'block mb-2 text-xs font-bold tracking-wide uppercase';
+    let inputClasses = 'w-full px-3 py-2 leading-tight border rounded shadow';
+  
+    if (invalid) {
+        labelClasses += ' text-red-400';
+        inputClasses += ' text-red-500 bg-red-100 border-red-300';
+    } else {
+        labelClasses += ' text-stone-300';
+        inputClasses += ' text-gray-700 bg-stone-300';
+    }
+  
+    return (
+        <p>
+            <label className={labelClasses}>{label}</label>
+            <input className={inputClasses} {...props} />
+        </p>
+    );
+}
+
